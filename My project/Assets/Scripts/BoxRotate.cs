db83274e06587b5299713bae2500d5a662b719c0
Ever_ <joohan1822@gmail.com>
2025-07-24 16:36:20 +0900

    2025.07..24
    Unity 기초 1일차

---
+++ My project/Assets/Scripts/BoxRotate.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class BoxRotate : MonoBehaviour
+{
+
+    public Vector3 pos;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(pos*Time.deltaTime);
+
+
+    }
+}
---
+++ My project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        speed = 5;
+        rb = GetComponent<Rigidbody>();
+        Debug.Log("dddd");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movemont = new Vector3(horizontal, 0, vertical);
+
+        rb.AddForce(movemont * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("ItemBox"))
+        {
+            Debug.Log("æ∆¿Ã≈€ »πµÊ");
+            other.gameObject.SetActive(false);
+        }
+        //if (other.gameObject.CompareTag("Obstacle"))
+        //{
+        //    Debug.Log("√Êµπ");
+        //    speed--;
+        //    other.gameObject.SetActive(false);
+        //}
+
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("√Êµπ");
+            speed--;
+            other.gameObject.SetActive(false);
+        }
+    }
+}
